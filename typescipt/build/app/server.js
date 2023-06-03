a1dffee61a57ebc96778907f9dab90c4f73c4e21
Shane Bekker <shaneb@datanamix.com>
2023-06-03 19:43:04 +0200

    Script Updates

--- typescipt/build/app/server.js
+++ typescipt/build/app/server.js
@@ -27,6 +27,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+// @ts-ignore
 const MasterRoute_1 = require("./routes/MasterRoute");
 const mongoose = __importStar(require("mongoose"));
 const dotenv_1 = __importDefault(require("dotenv"));
